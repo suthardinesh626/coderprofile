001d5cd9f5f1d3ee6032e4664e466e323d4d3bd9
Dinesh Suthar <suthardinesh626@gmail.com>
2023-10-21 17:46:39 +0530

    navbar fixed

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,12 +7,11 @@ const AboutMe = () => {
         <>
             <div className='about-detail' id='Home'>
                 <div className='deatil-text'>
-                <h1 >Hello, I'm Dinesh Suthar</h1>
-                <h2>a skilled React Developer proficient in ReactJS, Redux, HTML, CSS (including Tailwind CSS), SQL, and JavaScript. With a keen eye for design, I specialize in creating dynamic and responsive user interfaces. I bring a problem-solving mindset and a commitment to clean, scalable code to every project. My goal is to contribute to innovative development projects, leveraging my expertise to deliver impactful and user-friendly digital experiences. </h2>
-
+                    <h1 id='' >Hello, I'm Dinesh Suthar</h1>
+                    <h2>a skilled React Developer proficient in ReactJS, Redux, HTML, CSS (including Tailwind CSS), SQL, and JavaScript. With a keen eye for design, I specialize in creating dynamic and responsive user interfaces. I bring a problem-solving mindset and a commitment to clean, scalable code to every project. My goal is to contribute to innovative development projects, leveraging my expertise to deliver impactful and user-friendly digital experiences. </h2>
                 </div>
                 <div className='gitQR-link'>
-                    <img src={gitQR}  />
+                    <img src={gitQR} />
                 </div>
             </div>
         </>
